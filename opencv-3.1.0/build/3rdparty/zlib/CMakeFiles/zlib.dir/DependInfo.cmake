d500a01d458716a12198a64a27508e861ae26cca
udayan14 <14udayanjoshi@gmail.com>
2016-06-27 23:23:03 +0530

    First commit

---
+++ opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/adler32.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/adler32.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/compress.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/compress.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/crc32.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/crc32.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/deflate.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/deflate.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/gzclose.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/gzclose.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/gzlib.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/gzlib.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/gzread.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/gzread.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/gzwrite.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/gzwrite.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/infback.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/infback.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/inffast.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/inffast.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/inflate.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/inflate.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/inftrees.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/inftrees.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/trees.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/trees.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/uncompr.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/uncompr.c.o"
+  "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/zlib/zutil.c" "/Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/zlib/CMakeFiles/zlib.dir/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "3rdparty/zlib"
+  "../3rdparty/zlib"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ opencv-3.1.0/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0
+# Build directory: /Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(3rdparty/zlib)
+subdirs(3rdparty/libtiff)
+subdirs(3rdparty/libjpeg)
+subdirs(3rdparty/libwebp)
+subdirs(3rdparty/libjasper)
+subdirs(3rdparty/libpng)
+subdirs(3rdparty/openexr)
