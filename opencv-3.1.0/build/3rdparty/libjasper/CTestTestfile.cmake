12b8e127136f362c50acf064e07a1fe344d90c93
udayan14 <14udayanjoshi@gmail.com>
2016-06-27 23:40:35 +0530

    removed entire library which was miatakenly pushed

--- opencv-3.1.0/build/3rdparty/libjasper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/3rdparty/libjasper
-# Build directory: /Users/udayanjoshi/Desktop/Abhyas/Project/opencv-3.1.0/build/3rdparty/libjasper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
